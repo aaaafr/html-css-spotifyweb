21fd5455a95cf3bd3514491633998bdee35de37b
aaaafr <anastasia.frassini@icloud.com>
2021-04-21 19:38:25 +0200

    Js

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+
+
+// writeline
+document.writeln("Hello World");
+
+// alert
+alert("spotify 35#");
+
+// getelement
+document.getElementById("heavy_metal").innerHTML="Black Sabbath"
+
+// getelement
+document.getElementById("nuvole_bianche").innerHTML="Nuvole Bianche"
+
+// get element 
+document.getElementById("summertime").innerHTML="Summertime Sadness"
+
